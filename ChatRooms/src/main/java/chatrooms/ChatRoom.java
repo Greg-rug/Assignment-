ef06b813925bb7bf7da27eca18565bd4623f741b
Lubor <luboroo@gmail.com>
2020-09-22 20:15:42 +0200

    studs

---
+++ ChatRooms/src/main/java/chatrooms/ChatRoom.java
@@ -0,0 +1,15 @@
+package chatrooms;
+
+public class ChatRoom {
+
+    int portNumber;
+
+    public ChatRoom() {
+        askPortNumber();
+    }
+
+    private int askPortNumber() {
+
+        return 0;
+    }
+}
---
+++ ChatRooms/src/main/java/chatrooms/MainServer.java
@@ -0,0 +1,25 @@
+package chatrooms;
+
+import java.util.ArrayList;
+
+public class MainServer {
+
+    ArrayList <ChatRoom> chatrooms;
+    ArrayList <Integer> portNumbersUsed;
+
+    public MainServer() {
+        chatrooms = new ArrayList<>();
+        portNumbersUsed = new ArrayList<>();
+    }
+
+    public int allocatePortNumber() {
+
+        return 0;
+    }
+
+    private int generatePortNumber() {
+
+        return 0;
+    }
+
+}
