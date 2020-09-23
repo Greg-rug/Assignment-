aab4842dbb7c4b961c781547d52baf0be5c6a132
Greg-rug <62098552+Greg-rug@users.noreply.github.com>
2020-09-23 18:02:07 +0200

    Update MainServer.java

--- ChatRooms/src/main/java/chatrooms/MainServer.java
+++ ChatRooms/src/main/java/chatrooms/MainServer.java
@@ -1,25 +1,53 @@
 package chatrooms;
 
 import java.util.ArrayList;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
 
-public class MainServer {
-
-    ArrayList <ChatRoom> chatrooms;
-    ArrayList <Integer> portNumbersUsed;
-
-    public MainServer() {
-        chatrooms = new ArrayList<>();
-        portNumbersUsed = new ArrayList<>();
+public class MainServer{
+  protected ArrayList < User > userList;
+  protected ArrayList < ChatRoom > chatRooms;
+  protected ArrayList < Integer > portNumbersUsed;
+  public ServerSocket serverSocket;
+  private int threadNr;
+  public class MainServer (){
+    this.userList = new ArrayList < User > ();
+    this.chatRooms = new ArrayList < Chatroom > ();
+    try{
+      this.serverSocket = new ServerSocket (1234);
+    } catch (IOException e){
+      e.printStackTrace ();
     }
+    threadNr = 1;
 
-    public int allocatePortNumber() {
-
-        return 0;
+  }
+  public void createUser (){
+    try{
+      Socket userSocket = serverSocket.accept ();
+      System.out.println ("Spawning thread: " + threadNr);  
+      Thread user = new Thread (new User (userSocket, threadNr));
+      threadNr++;
+      user.start ();
+    } catch (IOException e){
+      e.printStackTrace ();
     }
-
-    private int generatePortNumber() {
-
-        return 0;
-    }
-
+  }
+  private int generatePortNumber() {
+    Random rand = new Random();
+    int resRandom = rand.nextInt((9999 - 100) + 1) + 10;
+    return resRandom;
+  }
+  protected Chatroom createChatRoom (String roomName, int portNumber){
+    this.portNumber = generatePortNumber();    
+    Chatroom room = new Chatroom (roomName, this.portNumber);
+    this.chatRooms.add (room);
+    this.portNumbersUsed.add (portNumber);
+    return room;
+  }
+  public static void main (String args[]){
+    MainServer server = new MainServer ();
+    server.start();
+  }
 }
