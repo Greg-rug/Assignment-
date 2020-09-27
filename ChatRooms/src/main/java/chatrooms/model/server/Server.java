45293409831a63da7b2440945934c294e42fc774
Lubor <luboroo@gmail.com>
2020-09-27 23:15:57 +0200

    demo comments

--- ChatRooms/src/main/java/chatrooms/model/server/Server.java
+++ ChatRooms/src/main/java/chatrooms/model/server/Server.java
@@ -21,10 +21,11 @@ public class Server {
     private final MessageFeed messageFeed;
     private final ExecutorService executorService;
 
-    /*
+    /* DEMO questions
      * constants,
      * executor service for each program
      * after clicking exit in chatroom remove port number from main server
+     * switch statement in bot manager
      * minimalistic gui
      * bot inheritance
      * server shutting down
@@ -34,7 +35,6 @@ public class Server {
      * gui elements in model
      * console error output for bot manager
      * is bot manager supposed to shut down after kill all?
-     *
     */
 
     public Server() {
