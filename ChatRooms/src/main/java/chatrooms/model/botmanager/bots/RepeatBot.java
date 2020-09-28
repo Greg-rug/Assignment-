3844ec63908aaa69414aca05c56d228d8151e35f
Greg-rug <62098552+Greg-rug@users.noreply.github.com>
2020-09-28 21:27:36 +0200

    Update RepeatBot.java

--- ChatRooms/src/main/java/chatrooms/model/botmanager/bots/RepeatBot.java
+++ ChatRooms/src/main/java/chatrooms/model/botmanager/bots/RepeatBot.java
@@ -21,4 +21,14 @@ public class RepeatBot extends Bot {
         return line;
     }
 
+    @Override
+    public void run() {
+        try {
+            out.println("I am - " + name + " and I shall repeat whatever you type . Enter BYE to exit");
+            String line;
+            while ((line = in.readLine()) != null && !line.trim().equalsIgnoreCase("BYE")) {                  
+			send(line);
+                }
+        } 
+    }
 }
