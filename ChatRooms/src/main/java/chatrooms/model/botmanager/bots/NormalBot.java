bc23401c94e6ec7458f8e434d901fdfb0c0249fa
Greg-rug <62098552+Greg-rug@users.noreply.github.com>
2020-09-28 21:26:47 +0200

    Update NormalBot.java

--- ChatRooms/src/main/java/chatrooms/model/botmanager/bots/NormalBot.java
+++ ChatRooms/src/main/java/chatrooms/model/botmanager/bots/NormalBot.java
@@ -7,10 +7,47 @@ public class NormalBot extends Bot {
     public NormalBot(String name, boolean isLocalBot) {
         super(name, isLocalBot);
     }
+       
+    final static int nrWords = 5;	
+    final static int lenghtSentence = 10;	
+    final static String SPACE = " ";	
+    final static String PERIOD = ".";     		
 
-    @Override
-    public String nextString() {
-        return ("I am totally normal - " + name);
+    static Random r = new Random();
+    public String RandomSentence(){
+	String Lines = messageFeed.getPreviousMessages(rand());
+	String[] Words = Lines.split(" ", Lines.length());	
+        String sentence;
+	sentence = Words[rand()];
+        char c = sentence.charAt(0);
+        sentence = sentence.replace( c, Character.toUpperCase(c) );
+    
+	for (int i = 0; i < lenghtSentence; i++){
+        sentence += SPACE + Words[rand()] ;
+	}
+
+        sentence += PERIOD;
+        return sentence ;
+	sentence = "";
+
     }
+    static int rand(){
+        int ri = r.nextInt() % nrWords;
+        if ( ri < 0 )
+            ri += nrWords;
+        return ri;
+    }
+    @Override 
+     public void run() {
+        try {
+            out.println("I am - " + name + "I shall talk your ear off with random nonsense . Enter BYE to exit");
+	    String reply = RandomSentence ;
+            String line;
+            while ((line = in.readLine()) != null && !line.trim().equalsIgnoreCase("BYE")) {
+			 messageFeed.setMessage(line);               
+			 send(reply);
+            }
+        } 
+     }
 
 }
